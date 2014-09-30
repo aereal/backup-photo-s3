d108a5eca58a685d58c543e23bf501df9e909865
aereal <aereal@aereal.org>
2014-09-30 22:56:04 +0900

    Move *.json into `data/`

--- bin/init_s3_backup.bash
+++ bin/init_s3_backup.bash
@@ -2,11 +2,15 @@
 
 set -ue
 
+# local
+script_dir="$(dirname "$0")"
+data_dir="$(realpath "$script_dir/../data")"
+
 export AWS_CONFIG_FILE="$HOME/.aws/config"
 export AWS_CMD="aws"
 export S3_REGION="ap-northeast-1"
 export S3_BUCKET="${S3_REGION}-aereal-photo-backup"
-export S3_LIFECYCLE_JSON="lifecycle.json"
+export S3_LIFECYCLE_JSON="${data_dir}/lifecycle.json"
 
 ${AWS_CMD} s3 mb s3://${S3_BUCKET} --region ${S3_REGION}
 ${AWS_CMD} s3api put-bucket-lifecycle --bucket "${S3_BUCKET}" --lifecycle file://${S3_LIFECYCLE_JSON}
